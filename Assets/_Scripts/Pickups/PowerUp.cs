a88bbac70010961f1e5f88f480f91bbe0cfc7358
cpg55850 <cpg55850@ucmo.edu>
2022-04-25 00:45:33 -0500

    Added sound FX for pickup, sprite work for health and helper

--- Assets/_Scripts/Pickups/PowerUp.cs
+++ Assets/_Scripts/Pickups/PowerUp.cs
@@ -13,7 +13,7 @@ public abstract class PowerUp : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
+        FindObjectOfType<AudioManager>().Play("Pickup");
         Pickup(collision);
     }
 
